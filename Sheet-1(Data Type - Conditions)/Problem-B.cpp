4575821b35ed64a8a8dc438123e6a753687127a7
GitGuru420 <raisulislam1892@gmail.com>
2024-10-06 14:03:32 +0530

    B - Basic Data Teypes

---
+++ Sheet-1(Data Type - Conditions)/Problem-B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int int_value;
+    long long long_value;
+    char char_value;
+    float float_value;
+    double double_value;
+
+    // Take variable value
+    cin >> int_value >> long_value >> char_value >> float_value >> double_value;
+    // Print variable value
+    cout << int_value << endl << long_value << endl;
+    cout << char_value << endl << float_value << endl;
+    cout << double_value;
+    
+    return 0;
+}
