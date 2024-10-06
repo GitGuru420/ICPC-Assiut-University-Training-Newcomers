d2d2b8804b4069185ceb3cfaaeb353b25f73bbd8
GitGuru420 <raisulislam1892@gmail.com>
2024-10-06 14:19:37 +0530

    C - Simple Calculator

--- Sheet-1(Data Type - Conditions)/Problem-C.cpp
+++ Sheet-1(Data Type - Conditions)/Problem-C.cpp
@@ -16,3 +16,23 @@ int main() {
 
     return 0;
 }
+
+// C Code
+/*
+    #include <stdio.h>
+    int main() {
+        long long x, y;
+        scanf("%lld %lld", &x, &y);
+
+        // summation
+        printf("%lld + %lld = %lld\n", x, y, (x+y));
+
+        // multiplication
+        printf("%lld * %lld = %lld\n", x, y, (x*y));
+
+        // subtraction
+        printf("%lld - %lld = %lld\n", x, y, (x-y));
+
+        return 0;
+    }
+*/
