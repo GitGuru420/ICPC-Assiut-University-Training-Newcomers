a2d98f9ce4c0266513d0067bc78c7cf7de3702ad
GitGuru420 <raisulislam1892@gmail.com>
2024-10-06 14:17:26 +0530

    C - Simple Calculator

---
+++ Sheet-1(Data Type - Conditions)/Problem-C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x, y;
+    cin >> x >> y;
+
+    // summation
+    cout << x << " + " << y << " = " << x + y << endl;
+
+    // multiplication
+    cout << x << " * " << y << " = " << x * y << endl;
+
+    // subtraction
+    cout << x << " - " << y << " = " << x - y << endl;
+
+    return 0;
+}
