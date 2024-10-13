a7020884b3da98308dc6b7674ea146bebc9b2104
GitGuru420 <raisulislam1892@gmail.com>
2024-10-13 15:27:57 +0530

    P - Shape1

--- Sheet-2(Loops)/Problem-P.c
+++ Sheet-2(Loops)/Problem-P.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main() {
     int n;
@@ -5,6 +6,23 @@ int main() {
 
     for(int i=n; i>=1; i--) {
         for(int j=1; j<=i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+*/
+
+// Shape1 - My Code
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++) {
+        for(int j=i; j<=n; j++) {
             printf("*");
         }
         printf("\n");
