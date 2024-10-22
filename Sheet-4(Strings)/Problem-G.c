0579d56c72a37b28cb975a0f2fd0af26492ee67a
GitGuru420 <raisulislam1892@gmail.com>
2024-10-22 21:18:44 +0530

    G - Conversion

---
+++ Sheet-4(Strings)/Problem-G.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char s[100001];
+    scanf("%s", &s);
+
+    int len = strlen(s);
+    for(int i=0; i<len; i++) {
+        if(s[i] == ',') {
+            printf(" ");
+        }
+        else if(s[i] >= 'a' && s[i] <= 'z') {
+            printf("%c", s[i] - 32);
+        }
+        else {
+            printf("%c", s[i] + 32);
+        }
+    }
+
+    return 0;
+}
